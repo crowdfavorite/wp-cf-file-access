497927da0b067278be331ddcae32adb35c34f1d8
sklein <sklein@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-09-21 21:13:18 +0000

    updating the plugin with small code changes, also updating changelog and readme with more information
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-file-access/trunk@39826 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-file-access.php
+++ cf-file-access.php
@@ -3,17 +3,20 @@
 Plugin Name: CF File Access 
 Plugin URI: http://crowdfavorite.com 
 Description: Process incoming /files/* requests for designated file extensions for pre-processing (ie: authentication, redirect, etc...) 
-Version: 1.0.1
+Version: 1.1
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
 
-// 	ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
+// Plugin Setup
+	
+	// 	ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
 
-load_plugin_textdomain('crowdfavorite');
-add_action('generate_rewrite_rules','cfap_addRewriteRules');
-add_action('query_vars','cfap_query_vars');
-add_action('template_redirect','cfap_deliver_file'); // this is the earliest we can find the wp_rewrite result
+	add_action('generate_rewrite_rules','cfap_addRewriteRules');
+	add_action('query_vars','cfap_query_vars');
+	add_action('template_redirect','cfap_deliver_file'); // this is the earliest we can find the wp_rewrite result
+	
+	load_plugin_textdomain('cfap');
 
 // Sample Overrides
 	
@@ -40,18 +43,22 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 	 */
 	function cfap_deliver_file() {
 		global $cfap_filepath;
-		if(is_null($cfap_filepath)) { return false; }
+		if (is_null($cfap_filepath)) { return false; }
 		
 		$deliver = true;		
-		$deliver = apply_filters('cfap_deliver_file',$deliver,$cfap_filepath);
+		$deliver = apply_filters('cfap_deliver_file', $deliver, $cfap_filepath);
 		
-		if(!$deliver) { cfap_denied($cfap_filepath); }
-		else { cfap_passthru($cfap_filepath); }
+		if (!$deliver) { 
+			cfap_denied($cfap_filepath); 
+		}
+		else { 
+			cfap_passthru($cfap_filepath); 
+		}
 	}
 	
 	/**
-	 * Deliver a 401 authentication error
-	 * @TODO: wordpress page generation to show a "purty" page
+	 * Deliver a WordPress formatted page that displays a Filterable error message
+	 * 
 	 * @param string $file - name of file that was requested
 	 */
 	function cfap_denied($file) {
@@ -72,20 +79,21 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 			
 		global $wp_query;
 		
+		// Setup the new Post Data
 		$post = new stdClass;
-			$post->ID = -1;
-			$post->post_author = 1;
-			$post->post_name = 'access-denied';
-			$post->guid = get_bloginfo('wpurl') . '/' . $post->name;
-			$post->post_title = 'Access Denied';
-			$post->post_content = '<p><b>Access denied to file: '.basename($file).'</b></p>';
-			$post->post_date = $post->post_date_gmt= date('Y-m-d H:i:s');
-			$post->post_category = '';
-			$post->post_excerpt = '';
-			$post->post_status = 'post';
-			$post->comment_status = 'closed';
-			$post->ping_status = 'closed';
-			$post->comment_count = 0;
+		$post->ID = -1;
+		$post->post_author = 1;
+		$post->post_name = 'access-denied';
+		$post->guid = site_url($post->name);
+		$post->post_title = 'Access Denied';
+		$post->post_content = '<p><b>Access denied to file: '.basename($file).'</b></p>';
+		$post->post_date = $post->post_date_gmt= date('Y-m-d H:i:s');
+		$post->post_category = '';
+		$post->post_excerpt = '';
+		$post->post_status = 'post';
+		$post->comment_status = 'closed';
+		$post->ping_status = 'closed';
+		$post->comment_count = 0;
 			
 		$wp_query->is_404 = false;
 		$wp_query->is_home = false;
@@ -100,9 +108,9 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 		
 		unset($wp_query->query['error']);
 		$wp_query->query_vars['error'] = '';
-		$wp_query->posts[] = apply_filters('cfap_denied_post',$post);
+		$wp_query->posts[] = apply_filters('cfap_denied_post', $post, $file);
 		
-		//die(__('401 &#8212; Access to file "'.basename($file).'" not allowed.'));	
+		remove_filter('the_content', 'wpautop');
 	}
 	
 	/**
@@ -111,7 +119,7 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 	 * @param string $file - name of file that was requested
 	 */
 	function cfap_not_found($file) {
-		do_action('cfap_not_found',$file);
+		do_action('cfap_not_found', $file);
 		return true;
 	}
 
@@ -120,16 +128,16 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 	 * @param string $file - name of file that was requested
 	 */
 	function cfap_passthru($file) {
-		do_action('cfap_passthru',$file);
+		do_action('cfap_passthru', $file);
 		
-		$file = apply_filters('cfap_filepath',$file);
+		$file = apply_filters('cfap_filepath', $file);
 
 		// make sure our file exists
-		if(!is_file($file)) {
+		if (!is_file($file)) {
 			// path is not absolute, see if we have a file in the uploads dir
 			$file = ABSPATH.UPLOADS.$file;
 			// no file found, send 404
-			if(!is_file($file)) {
+			if (!is_file($file)) {
 				return cfap_not_found($file);
 			}
 		}
@@ -138,11 +146,11 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 	
 		// a lot of the rest of this is from blogs.php, but slightly modded where deemed necessary
 		$mime = wp_check_filetype($file);
-		if($mime['type'] === false && function_exists('mime_content_type')) {
+		if ($mime['type'] === false && function_exists('mime_content_type')) {
 			$mime['type'] = mime_content_type($file);
 		}
 
-		if($mime['type'] != false) {
+		if ($mime['type'] != false) {
 			$mimetype = $mime[ 'type' ];
 		} else {
 			$ext = pathinfo($file,PATHINFO_EXTENSION);
@@ -157,6 +165,8 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 		// the rest is unmodified form blogs.php
 		@header( 'Content-type: ' . $mimetype ); // always send this
 		@header( 'Content-Length: ' . filesize( $file ) );
+		
+		@header('Content-disposition: inline; filename='.basename($file));		
 		
 		$last_modified = gmdate('D, d M Y H:i:s', filemtime( $file ));
 		$etag = '"' . md5($last_modified) . '"';
@@ -177,42 +187,42 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 		// Make a timestamp for our most recent modification...	
 		$modified_timestamp = strtotime($last_modified);
 		
-		if ( ($client_last_modified && $client_etag) ?
+		if (($client_last_modified && $client_etag) ?
 			 (($client_modified_timestamp >= $modified_timestamp) && ($client_etag == $etag)) :
 			 (($client_modified_timestamp >= $modified_timestamp) || ($client_etag == $etag)) ) {
-			status_header( 304 );
+			status_header(304);
 			exit;
 		}
 		
 		// If we made it this far, just serve the file
-		readfile( $file );
-		// forcefully exit, needed to stop the rest of wordpress from processing
+		readfile($file);
+		// forcefully exit, needed to stop the rest of WordPress from processing
 		exit;
 	}
 
 // Rewrite links
 
 	/**
-	 * check to make sure that our permalink structure is present
-	 * null the field if it is not - this will force wp-rewrite to recalculate the rewrite rules
+	 * Check to make sure the Rewrite rules are in place for us to function.  If not reset the rules so they get rebuilt
 	 *
 	 * @param array $value 
-	 * @return string/array
+	 * @return array
 	 */
 	function cfap_get_rewrite_option($value) {
 		$rules = cfap_get_rewrite_rules();
-		foreach(array_keys($rules) as $rule) {
-			if(!array_key_exists($rule,$value)) {
+		foreach (array_keys($rules) as $rule) {
+			if (!array_key_exists($rule,$value)) {
 				$value = '';
 			}			
 		}
 		return $value;
 	}
-	add_filter('option_rewrite_rules','cfap_get_rewrite_option',9999);
+	add_filter('option_rewrite_rules', 'cfap_get_rewrite_option', 9999);
 	
 	/**
-	 * Default catch all redirected files
-	 * Which files should be redirected should be caught in the .htaccess file
+	 * Add the Rewrite rules to the global $wp_rewrite object
+	 *
+	 * @return void
 	 */
 	function cfap_addRewriteRules() {
 		global $wp_rewrite;
@@ -220,6 +230,11 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 		$wp_rewrite->rules = $new_rules + $wp_rewrite->rules;		
 	}
 	
+	/**
+	 * Rewrite rules that need to be in place for plugin to load properly
+	 *
+	 * @return array
+	 */
 	function cfap_get_rewrite_rules() {
 		global $wp_rewrite;
 		return array(
@@ -230,6 +245,7 @@ add_action('template_redirect','cfap_deliver_file'); // this is the earliest we 
 	
 	/**
 	 * Make our detection var public domain
+	 * 
 	 * @return array - modified query vars array
 	 */
 	function cfap_query_vars($vars){
